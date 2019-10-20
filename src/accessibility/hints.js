8ebab104596c47d8dc26177c6ebec5c5d4a2c484
Lukasz Nawrocki <lukasz.w.nawrocki@gmail.com>
2019-10-20 21:26:25 +0200

    Hints file added

---
+++ src/accessibility/hints.js
@@ -0,0 +1,6 @@
+export const EmailInputHint = 'Enter your email here.'
+export const PasswordInputHint =
+  'Enter your password here. It must consist of at least 8 characters and include at least one lowercase letter, one uppercase letter and one digit.'
+export const CheckboxButtonHint =
+  'Check it to confirm you have read and accept terms and conditions.'
+export const CheckboxLabelHint = 'I have read and accept terms and conditions.'
